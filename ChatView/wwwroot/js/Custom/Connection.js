267c106605ad2606acbfd8f9fe34d52476bda1ba
Nasir khalil <nasirkhalil953@gmail.com>
2020-03-29 15:40:19 +0500

    Added header and footer in shared view

--- ChatView/wwwroot/js/Custom/Connection.js
+++ ChatView/wwwroot/js/Custom/Connection.js
@@ -1,13 +1,12 @@
 ﻿var connection = new signalR.HubConnectionBuilder()
     .withUrl("/MyHub")
     .build();
-//var userguid = $("#userguid").val();
-//var userrole = $("#userrole").val();
-//var globalguid = "0705a73a-f95b-40c8-8ab9-f5956feb6334";
+var userguid = $("#currentuserid").val();
 connection.start().then(function () {
-    //if (userrole == "1") {
-    //    connection.invoke("JoinMyHub", globalguid);
-    //}
+       connection.invoke("JoinMyHub", userguid);
+});
+$(window).on('beforeunload', function () {
+    connection.invoke("LeaveMyHub", userguid);
 });
 window.play = function () {
     var audioElement = document.createElement('audio');
--- ChatView/wwwroot/js/Custom/Dashboard.js
+++ ChatView/wwwroot/js/Custom/Dashboard.js
@@ -3,20 +3,44 @@
         type: "GET",
         url: "/Dashboard/GetAllUsers",
         success: function (result) {
-            console.log(result);
-            debugger
-            
+            //console.log(result);
+            $("#customers").html('');
+            $.each(result, function (i, v) {
+                if (v.id!=userguid) {
+                    var area = CreateUserArea(v);
+                    $("#customers").append(area);
+                }
+            });
+
         },
         error: function (xhr) {
         }
     });
 }
+function CreateUserArea(v) {
+    var area = '<a href="Javascript:void(0)" onclick=ThisUser(this) id="CustomerNo' + v.id + '" data-id=' + v.id + ' data-firstname=' + v.firstName + ' data-lastname='+v.lastName+' data-customercode='+v.customerCode+'>' +
+        '<hr class="mt-0">' +
+        '<h5 class="pl-3 pr-3">'+v.firstName+ " "+ v.lastName+'<span class="float-right badge badge-pill badge-danger" id="ChatCounter'+v.id+'">25</span></h5 >' +
+        '<hr class="mb-0">' +
+        '</a>';
+    return area;
+}
+function ThisUser(elem) {
+    var id = $(elem).data('id');
+    var firstname = $(elem).data('firstname');
+    var lastname = $(elem).data('lastname');
+    var customercode = $(elem).data('customercode');
+    debugger
+
+} 
+
 function LogOut() {
     $.ajax({
         type: "POST",
         url: "/Login/LogOut",
         success: function (data) {
             if (data === "success") {
+                connection.invoke("LeaveMyHub", userguid);
                 window.location.href = ('/Login/Index');
             }
         },
